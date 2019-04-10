3850269570b32402102c1c064630f4feec861439
jellybeantatai <abhinabakar@gmail.com>
2019-04-10 10:22:53 +0530

    made store.js

--- src/store.js
+++ src/store.js
@@ -4,3 +4,48 @@ import "firebase/firestore";
 import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
 import { reduxFirestore, firestoreReducer } from "redux-firestore";
 //Reducers
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTunoHIUdS6wNaetO0p9Bh6BQUVb7DBd4",
+  authDomain: "react-owesome-3a46f.firebaseapp.com",
+  databaseURL: "https://react-owesome-3a46f.firebaseio.com",
+  projectId: "react-owesome-3a46f",
+  storageBucket: "react-owesome-3a46f.appspot.com",
+  messagingSenderId: "1023898337669"
+};
+
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true
+};
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig);
+
+//Init Firestore
+const firestore = firebase.firestore();
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig),
+  reduxFirestore(firebase)
+)(createStore);
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
+});
+
+// Create store with reducers and initial state
+const initialState = {};
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
